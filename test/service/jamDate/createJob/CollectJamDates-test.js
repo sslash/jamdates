8e1ab26522d4938f8aaeb2f9fac1ee32a5819b00
sslash <michaelgunnulfsen@gmail.com>
2016-11-22 17:13:46 +0100

    more testst

--- test/service/jamDate/createJob/CollectJamDates-test.js
+++ test/service/jamDate/createJob/CollectJamDates-test.js
@@ -18,10 +18,7 @@ const {
 } = require('./fixture');
 
 describe('Collect jam dates', () => {
-    it ('should create a blues group', () => {
-
-        // TODO: CONTINUE HERE and create tests for 
-        // having a map like this:
+    it ('should create a rap group', () => {
         /*
          * genreBuckets: {
          *      rap: [ {email: 'sap@mail.com', instrumentId: 10},{email: 'sap@mail.com', instrumentId: 11},{email: 'sap@mail.com', instrumentId: 12} ],
@@ -45,7 +42,49 @@ describe('Collect jam dates', () => {
             Object.keys(jamGroup).forEach(k => {
                 console.log(`[${k}]     `, jamGroup[k]);
             })
-        })
-
+        });
     });
+
+    it ('should create a rap, and a blues group', () => {
+
+        // TODO: duno if this test works. so test it
+
+        /*
+         * genreBuckets: {
+         *      rap: [ {email: 'sap@mail.com', instrumentId: 10},{email: 'sap@mail.com', instrumentId: 11},{email: 'sap@mail.com', instrumentId: 12} ],
+         *      metal: [ {email: 'sap4@mail.com', instrumentId: 11},{email: 'sa77p@mail.com', instrumentId: 11},{email: 'sapj@mail.com', instrumentId: 12} ],
+         * }
+         * 
+         */ 
+        const genreBuckets = new Map();
+        
+        const rapBucket = guitarists.slice(0, 3).concat(
+            bassPlayers.slice(1, 4)).concat(
+            drummers.slice(2, 5)).concat(
+            keyers.slice(3, 7));
+        
+        const bluesBucket = guitarists.slice(0, 1).concat(
+            bassPlayers.slice(4, 7)).concat(
+            drummers.slice(2, 8)).concat(
+            keyers.slice(6, 9));
+
+        genreBuckets.set(rap, rapBucket);
+        genreBuckets.set(blues, bluesBucket);
+
+        const jamGroups = new CollectJamDates(genreBuckets).collect();
+
+        jamGroups.forEach(jamGroup => {
+            console.log('\n');
+            Object.keys(jamGroup).forEach(k => {
+                console.log(`[${k}]     `, jamGroup[k]);
+            })
+        });
+    });
+
+    // TODO: make sure buckets are (in CreateJamDatesService) created like this.
+    // then do a new function: for every jamgroup that is complete,
+    // create a new jamgroup instance and set state to pending.
+    // send email to all participants and schedule a time for them.
+    // set user_jam_participation status to in-active. Make sure 
+    // in-active's arent included when we try to create jam groups (think I forgot that part) 
 });
